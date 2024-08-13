1b56252e35b557ba1c45640e923a9ecbd8efd43a
Ulan <ulantursunbekdev@gmail.com>
2024-08-13 11:17:08 +0600

    Setup code formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,81 @@
 module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  plugins: ["react-refresh", "simple-import-sort", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json", "./tsconfig.node.json", "./tsconfig.app.json"],
+    tsconfigRootDir: __dirname,
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "@typescript-eslint/no-floating-promises": "error",
+    "max-len": "off",
+    "object-curly-newline": "off",
+    "linebreak-style": "off",
+    "no-constant-condition": ["error", { checkLoops: false }],
+    "newline-before-return": "error",
+    "react/no-unescaped-entities": ["error", { forbid: [">", "}"] }],
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@typescript-eslint/no-floating-promises": ["warn"],
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/jsx-curly-brace-presence": [
+      "error",
+      { props: "never", children: "never" },
+    ],
+    "prettier/prettier": [
+      "error",
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  overrides: [
+    {
+      files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+      rules: {
+        "simple-import-sort/imports": [
+          "error",
+          {
+            groups: [
+              // `react` first, then packages starting with a character
+              ["^react$", "^[a-z]"],
+              ["^assets"],
+              // Packages starting with `@`
+              ["^@"],
+              // Packages starting with `~`
+              ["^~"],
+              // Imports starting with `../`
+              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+              // Imports starting with `./`
+              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+              // Style imports
+              ["^.+\\.s?css$"],
+              // Side effect imports
+              ["^\\u0000"],
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
